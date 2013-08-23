a9b6021265504588ee5a146459ba9f6a6e65eb1a
Junian <junian.triajianto@gmail.com>
2013-08-23 09:46:56 +0700

    init project

---
+++ iOSGoogleMapSample/JUNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JUNAppDelegate.h
+//  iOSGoogleMapSample
+//
+//  Created by あ on 13/08/23.
+//  Copyright (c) 2013年 Labora by Junian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JUNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSGoogleMapSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSGoogleMapSample
+//
+//  Created by あ on 13/08/23.
+//  Copyright (c) 2013年 Labora by Junian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JUNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JUNAppDelegate class]));
+    }
+}
