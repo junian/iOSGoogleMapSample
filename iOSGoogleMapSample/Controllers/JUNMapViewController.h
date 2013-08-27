9df9b06da11d417023c6cee49967ce1a6f711216
Junian <junian.triajianto@gmail.com>
2013-08-27 10:52:29 +0700

    added MapViewController

---
+++ iOSGoogleMapSample/Controllers/JUNMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  JUNMapViewController.h
+//  iOSGoogleMapSample
+//
+//  Created by あ on 13/08/27.
+//  Copyright (c) 2013年 Labora by Junian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JUNMapViewController : UIViewController
+
+@end
---
+++ iOSGoogleMapSample/Controllers/JUNMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  JUNMapViewController.m
+//  iOSGoogleMapSample
+//
+//  Created by あ on 13/08/27.
+//  Copyright (c) 2013年 Labora by Junian. All rights reserved.
+//
+
+#import "JUNMapViewController.h"
+
+@interface JUNMapViewController ()
+
+@end
+
+@implementation JUNMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
