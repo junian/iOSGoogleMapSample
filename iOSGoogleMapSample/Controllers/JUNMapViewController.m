38424748a9a1a7f7caa7d17f56e6424a13cdc3af
Junian <junian.triajianto@gmail.com>
2013-08-27 10:57:39 +0700

    added sample code

--- iOSGoogleMapSample/Controllers/JUNMapViewController.m
+++ iOSGoogleMapSample/Controllers/JUNMapViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "JUNMapViewController.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 @interface JUNMapViewController ()
 
 @end
 
 @implementation JUNMapViewController
+
+GMSMapView* mapView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,6 +30,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
+                                                            longitude:151.20
+                                                                 zoom:6];
+    mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView.myLocationEnabled = YES;
+    self.view = mapView;
+    
+    // Creates a marker in the center of the map.
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+    marker.title = @"Sydney";
+    marker.snippet = @"Australia";
+    marker.map = mapView;
 }
 
 - (void)didReceiveMemoryWarning
